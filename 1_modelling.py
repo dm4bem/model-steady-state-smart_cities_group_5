fa403a9fc51ced5edb3407fa07dff99c64be4105
dalleryb <166363898+dalleryb@users.noreply.github.com>
2024-05-24 09:08:40 +0200

    Update 1_modelling.py

--- 1_modelling.py
+++ 1_modelling.py
@@ -9,6 +9,19 @@ l = 4               # m length of the cubic room
 Sg = l**2           # m² surface area of one window
 Sc = Si = 5 * Sg    # m² surface area of concrete & insulation of the 5 walls
 
+concrete = {'Conductivity': 1.400,          # W/(m·K)
+            'Density': 2300.0,              # kg/m³
+            'Specific heat': 880,           # J/(kg⋅K)
+            'Width': 0.2,                   # m
+            'Surface': 5 * l**2}            # m²
+
+insulation = {'Conductivity': 0.027,        # W/(m·K)
+              'Density': 55.0,              # kg/m³
+              'Specific heat': 1210,        # J/(kg⋅K)
+              'Width': 0.08,                # m
+              'Surface': 5 * l**2}          # m²
+
 air = {'Density': 1.2,                      # kg/m³
        'Specific heat': 1000}               # J/(kg·K)
 pd.DataFrame(air, index=['Air'])
+
