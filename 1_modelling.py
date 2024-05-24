7341b5df653a94fb2c158af5b002cee35dae6056
panagioz <164942193+panagioz@users.noreply.github.com>
2024-05-24 09:02:16 +0200

    Update 1_modelling.py

--- 1_modelling.py
+++ 1_modelling.py
@@ -4,6 +4,11 @@ import matplotlib.pyplot as plt
 
 import dm4bem
 
-l = 3               # m length of the cubic room
-Sg = l**2           # m² surface area of the glass wall
+## Building properties
+l = 4               # m length of the cubic room
+Sg = l**2           # m² surface area of one window
 Sc = Si = 5 * Sg    # m² surface area of concrete & insulation of the 5 walls
+
+air = {'Density': 1.2,                      # kg/m³
+       'Specific heat': 1000}               # J/(kg·K)
+pd.DataFrame(air, index=['Air'])
