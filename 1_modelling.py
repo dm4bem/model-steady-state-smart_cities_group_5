9560e7b8cc8a167ade56277ec98cf45c309e2c75
panagioz <164942193+panagioz@users.noreply.github.com>
2024-05-24 10:48:10 +0200

    Update 1_modelling.py

--- 1_modelling.py
+++ 1_modelling.py
@@ -21,30 +21,37 @@ concrete = {'Conductivity': 1.400,          # W/(m·K)
             'Density': 2300.0,              # kg/m³
             'Specific heat': 880,           # J/(kg⋅K)
             'Width': 0.2,                   # m
-            'Surface': 5 * l**2}            # m²
+            'Surface': Sc}                  # m²
 
 insulation = {'Conductivity': 0.027,        # W/(m·K)
               'Density': 55.0,              # kg/m³
               'Specific heat': 1210,        # J/(kg⋅K)
               'Width': 0.08,                # m
-              'Surface': 5 * l**2}          # m²
+              'Surface': Sc}                # m²
 
-glass = {'Conductivity': 1.4,               # W/(m·K)
+glass = {'Conductivity': 2.8,               # W/(m²·K) - https://www.gov.scot/binaries/content/documents/govscot/publications/advice-and-guidance/2020/02/tables-of-u-values-and-thermal-conductivity/documents/6-a---tables-of-u-values-and-thermal-conductivity/6-a---tables-of-u-values-and-thermal-conductivity/govscot%3Adocument/6.A%2B-%2BTables%2Bof%2BU-values%2Band%2Bthermal%2Bconductivity%2B%2B.pdf
          'Density': 2500,                   # kg/m³
          'Specific heat': 1210,             # J/(kg⋅K)
-         'Width': 0.04,                     # m
-         'Surface': l**2}                   # m²
+         'Width': 1,                        # m - 1m because m² at conductivity
+         'Surface': Sw}                     # m²
+
+wood = {'Conductivity': 0.14,               # W/(m·K) - https://materials.ads.org.uk/larch/
+         'Density': 500,                    # kg/m³ - Larch wood; EngineeringToolbox.com
+         'Specific heat': 1500,             # J/(kg⋅K) - Larch wood; https://web.ornl.gov/sci/buildings/conf-archive/1992%20B5%20papers/028.pdf#:~:text=URL%3A%20https%3A%2F%2Fweb.ornl.gov%2Fsci%2Fbuildings%2Fconf
+         'Width': 0.05,                     # m
+         'Surface': Sd}                     # m²
 
 wall = pd.DataFrame.from_dict({'Layer_out': concrete,
                                'Layer_in': insulation,
-                               'Glass': glass},
+                               'Glass': glass, 
+                               'Wood': wood},
                               orient='index')
 
 
 # radiative properties
 ε_wLW = 0.85    # long wave emmissivity: wall surface (concrete)
 ε_gLW = 0.90    # long wave emmissivity: glass pyrex
-ε_dLW = 0.82    # long wave emmissivity: wood (general hardwood, Engineering Toolbox)
+ε_dLW = 0.82    # long wave emmissivity: wood (general hardwood, EngineeringToolbox.com)
 α_dSW = 0.8     # short wave absortivity: wood brown (Wufiwiki)
 α_wSW = 0.25    # short wave absortivity: white smooth surface
 α_gSW = 0.38    # short wave absortivity: reflective blue glass
@@ -55,4 +62,11 @@ wall = pd.DataFrame.from_dict({'Layer_out': concrete,
 print(f'σ = {σ} W/(m²⋅K⁴)')
 # Convection coefficient
 h = pd.DataFrame([{'in': 8., 'out': 25}], index=['h'])  # W/(m²⋅K)
+
+
+### Thermal Network
+# conduction
+G_cd = wall['Conductivity'] / wall['Width'] * wall['Surface']
+print(G_cd)
+pd.DataFrame(G_cd, columns=['Conductance'])
 
