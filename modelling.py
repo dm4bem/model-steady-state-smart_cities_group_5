82f7418df0da094919d2515ef1302b568b16257e
HeloiseDeredec <166340523+HeloiseDeredec@users.noreply.github.com>
2024-05-30 16:43:37 +0200

    Update modelling.py

--- modelling.py
+++ modelling.py
@@ -153,3 +153,67 @@ TC = {"A": A,
 
 
 dm4bem.print_TC(TC)
+
+# STEADY STATE STUDY
+
+# by default TC['G']['q11'] = 0, i.e. Kp -> 0, no controller (free-floating)
+if controller:
+    TC['G']['q7'] = 1e3        # Kp -> ∞, almost perfect controller
+
+# by default TC['G']['q11'] = 0, i.e. Kp -> 0, no controller (free-floating)
+        # Kp -> ∞, almost perfect controller
+
+if neglect_air_glass_capacity:
+    TC['C']['θ6'] = TC['C']['θ7'] = 0
+    # or
+    TC['C'].update({'θ6': 0, 'θ7': 0})
+# State-space
+[As, Bs, Cs, Ds, us] = dm4bem.tc2ss(TC)
+
+bss = np.zeros(8)        # temperature sources b for steady state
+bss[[0, 5]] = 10      # outdoor temperature
+bss[[7]] = 20            # indoor set-point temperature
+
+fss = np.zeros(6)         # flow-rate sources f for steady state
+A = TC['A']
+G = TC['G']
+diag_G = pd.DataFrame(np.diag(G), index=G.index, columns=G.index)
+
+θss = np.linalg.inv(A.T @ diag_G @ A) @ (A.T @ diag_G @ bss + fss)
+print(f'θss = {np.around(θss, 2)} °C')
+bss = np.zeros(8)        # temperature sources b for steady state
+
+fss = np.zeros(6)         # flow-rate sources f for steady state
+fss[[5]] = 100000
+
+θssQ = np.linalg.inv(A.T @ diag_G @ A) @ (A.T @ diag_G @ bss + fss)
+print(f'θssQ = {np.around(θssQ, 2)} °C')
+bT = np.array([10, 10, 20])     # [To, To, To, Tisp]
+fQ = np.array([0, 0, 0])         # [Φo, Φi, Qa, Φa]
+uss = np.hstack([bT, fQ])           # input vector for state space
+print(f'uss = {uss}')
+
+inv_As = pd.DataFrame(np.linalg.inv(As),
+                      columns=As.index, index=As.index)
+yss = (-Cs @ inv_As @ Bs + Ds) @ uss
+
+yss = float(yss.values[0])
+print(f'yss = {yss:.2f} °C')
+
+print(f'Error between DAE and state-space: {abs(θss[5] - yss):.2e} °C')
+
+bT = np.array([0, 0, 0])         # [To, To, To, Tisp]
+fQ = np.array([0, 0, 100000])      # [Φo, Φi, Qa, Φa]
+uss = np.hstack([bT, fQ])
+
+inv_As = pd.DataFrame(np.linalg.inv(As),
+                      columns=As.index, index=As.index)
+yssQ = (-Cs @ inv_As @ Bs + Ds) @ uss
+
+yssQ = float(yssQ.values[0])
+print(f'yssQ = {yssQ:.2f} °C')
+
+print(f'Error between DAE and state-space: {abs(θssQ[5] - yssQ):.2e} °C')
+
+
+
