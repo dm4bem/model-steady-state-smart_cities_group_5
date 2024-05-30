01002f2f72ce2b3dce595b9b3eacb54c5953e64c
panagioz <164942193+panagioz@users.noreply.github.com>
2024-05-30 09:23:29 -0600

    Update inputs.py

--- inputs.py
+++ inputs.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 from dm4bem import read_epw, sol_rad_tilt_surf
 
 
-#filename = './weather_data/FRA_AR_Grenoble.074850_TMYx.epw'
-filename = './weather_data/FRA_AR_Grenoble.Alpes.Isere.AP.074860_TMYx.2007-2021.epw'
+#filename = './FRA_AR_Grenoble.074850_TMYx.epw'
+filename = './FRA_AR_Grenoble.Alpes.Isere.AP.074860_TMYx.2007-2021.epw'
 
 
 [data, meta] = read_epw(filename, coerce_year=None)
